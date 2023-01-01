1a51484432289e8d6662a55d479959824265ff93
Kris <nyk.krishnan@gmail.com>
2023-01-01 22:52:44 +0530

    Initial Commit!

---
+++ InvMgmnt.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import ttk
+
+root=Tk()
+root.title("RIT Database Management System")
+
+tkdesigner "https://www.figma.com/file/D3bL0Y9pFIM52kPgFD7cZk/Inventory-Management-System?node-id=3%3A12&t=AzlFYHUUStrODeZI-1" figd_5kCquDQyhZtUJp96AfI30w1rCUJmy4Lje6s_GNVp
---
+++ build/from pathlib import Path.py
@@ -0,0 +1,113 @@
+from pathlib import Path
+
+# from tkinter import *
+# Explicit imports to satisfy Flake8
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\nykkr\Documents\AaactualDocuments\DBMS Project\InventoryManagementSystem\build\assets\frame0")
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+
+window = Tk()
+
+window.geometry("1440x1024")
+window.configure(bg = "#FED954")
+
+
+canvas = Canvas(
+    window,
+    bg = "#FED954",
+    height = 1024,
+    width = 1440,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+canvas.create_rectangle(
+    0,
+    0.0,
+    200.0,
+    1024.0,
+    fill="#EFDEA3",
+    outline="")
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_1 clicked"),
+    relief="flat"
+)
+button_1.place(
+    x=30.0,
+    y=947.0,
+    width=153.0,
+    height=46.0
+)
+
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    106.0,
+    859.0,
+    image=image_image_1
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_2 clicked"),
+    relief="flat"
+)
+button_2.place(
+    x=0.0,
+    y=250.0,
+    width=198.0,
+    height=61.0
+)
+
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+button_3 = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_3 clicked"),
+    relief="flat"
+)
+button_3.place(
+    x=0.0,
+    y=172.0,
+    width=198.0,
+    height=61.0
+)
+
+button_image_4 = PhotoImage(
+    file=relative_to_assets("button_4.png"))
+button_4 = Button(
+    image=button_image_4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_4 clicked"),
+    relief="flat"
+)
+button_4.place(
+    x=0.0,
+    y=94.0,
+    width=198.0,
+    height=61.0
+)
+window.resizable(False, False)
+window.mainloop()
